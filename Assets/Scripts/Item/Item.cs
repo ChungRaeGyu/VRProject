a08ba3e47e981875eac5a14e34bd5b530e04b90c
JongHyuk Yoon <mana951@naver.com>
2023-06-20 10:01:11 +0900

    yoon commit
    열쇠, Cutter 자막 테스트 완료

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
     GameObject Player;
-    void Start() {
+    Text FullTxt;
+    private bool Full;
+
+    void Start()
+    {
+        Full = true;
         Player = GameObject.Find("OVRPlayerController");
+        FullTxt = GameObject.Find("FullHealth").GetComponent<Text>();
     }
     IEnumerator UseHealth()
     {
@@ -14,6 +21,12 @@ public class Item : MonoBehaviour
         Player.GetComponent<PlayerScript>().drawHealth();
         yield return null;
     }
+    IEnumerator FalseText()
+    {
+        FullTxt.GetComponent<Text>().enabled = false;
+        yield return new WaitForSeconds(2.0f);
+    }
+
 
     private void OnTriggerStay(Collider coll)
     {
@@ -21,8 +34,16 @@ public class Item : MonoBehaviour
         {
             if (OVRInput.GetDown(OVRInput.Button.Two)&&PlayerScript.PlayerHP<2)
             {
-                StartCoroutine(UseHealth());
-                Destroy(coll.gameObject);
+                if(PlayerScript.PlayerHP<2)
+                {
+                    StartCoroutine(UseHealth());
+                    Destroy(coll.gameObject);
+                }
+                else if(PlayerScript.PlayerHP==2)
+                {
+                    FullTxt.GetComponent<Text>().enabled = true;
+                    StartCoroutine(FalseText());
+                }
             }
         }
         
